14a64089f9dbffc5a57a781e690b5e66f3708757
170030745 <60337629+170030745@users.noreply.github.com>
2021-05-13 14:58:18 +0530

    Create StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,18 @@
+Public class StudentDetails
+{
+public static void main(String args[])
+{
+Student sravani=new Student();//created object student
+Student swathi=new Student();
+//instances are created
+sravani.setIdnumber(01);
+sravani.setMarks(69);
+sravani.setTotalsubjects(6);
+sravani.setDepartment("CSE");
+swathi.setIdnumber(02);
+swathi.setMarks(78);
+swathi.setTotalsubjects(8);
+swathi.setDepartment("ECE");
+}
+
+
