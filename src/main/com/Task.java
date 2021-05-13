e6a45d022b79bf26d490f6cff94e0778d9e61c55
170030745 <60337629+170030745@users.noreply.github.com>
2021-05-13 13:43:12 +0530

    Update and rename task.java to Task.java

---
+++ src/main/com/Task.java
@@ -0,0 +1,101 @@
+public class Emp
+{
+public static void main(String args[])
+{
+
+int empid=5;
+String empname="ABC";
+double empsal=50000.00d;
+float empheight=5.4f;
+short empexperience=3;
+}
+}
+
+
+public class Comments
+{
+public static void main(String args[])
+{
+int a,b,c;
+//single line
+/*int a=5;
+........
+multi line comments
+*/
+/** documentation Comments
+ * for html codes
+ * 
+ *
+ */
+
+}
+}
+
+public class Operators
+{
+public static void main(String args[])
+{
+int age=23;
+int sal=50000;
+if(sal>45000&&age<21)
+System.out.println("True");
+}
+}
+
+public class Example
+{
+public static void main(String args[])
+{
+int age=23;
+double sal=40000.0d;
+if(sal>45000||age<21)
+System.out.println("True");
+}
+}
+
+
+public class Marks
+{
+public static void main(String args[])
+{
+int maths=87;
+int science=78;
+int social=55;
+if(science>75&&maths>75&&social>50)
+System.out.println("pass");
+}
+}
+
+public class Marks
+{
+public static void main(String args[])
+{
+int maths=87;
+int science=78;
+int social=55;
+if((science>50&&maths>75)||social>=50)
+System.out.println("pass");
+}
+}
+public class Marks
+{
+public static void main(String args[])
+{
+int a=8;
+int b=5;
+int sum=0,mul=0;
+float div;
+sum=a+b;
+mul=a*b;
+div=a/b;
+System.out.println("sum is:"+sum);
+System.out.println("mul is:"+mul);
+System.out.println("div is:"+div);
+}
+}
+
+
+
+
+
+
